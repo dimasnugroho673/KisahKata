ac7ddc49be55074fa982787adc54a010b35aca53
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-07-31 22:39:16 +0700

    nambahin stories model

---
+++ KisahKata/Models/Stories.swift
@@ -0,0 +1,33 @@
+//
+//  Stories.swift
+//  KisahKata
+//
+//  Created by Dimas Putro on 31/07/21.
+//
+
+import Foundation
+import UIKit
+
+class Stories: Identifiable {
+    // Properties
+    let id: Int
+    let title: String
+    let coverImage: String
+    let stories: [String]
+    let storyIlustrations: [String]
+    let highlightedWords: [String]
+    let videoDetail: String
+    let descriptionDetail: String
+    
+    init(id: Int, title: String, coverImage: String, stories: [String], storyIlustrations: [String], highlightedWords: [String], videoDetail: String, descriptionDetail: String) {
+        self.id = id
+        self.title = title
+        self.coverImage = coverImage
+        self.stories = stories
+        self.storyIlustrations = storyIlustrations
+        self.highlightedWords = highlightedWords
+        self.videoDetail = videoDetail
+        self.descriptionDetail = descriptionDetail
+    }
+
+}
